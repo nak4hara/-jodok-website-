a65411f3c91235907756b110cec6d9894291d6a3
Karina <lkarina.nakahara@gmail.com>
2023-06-18 22:38:37 -0300

    project start

---
+++ jodok/src/App.jsx
@@ -0,0 +1,20 @@
+import Ramen from "./recipes/Ramen"
+
+export default function App() {
+
+  return (
+    <>
+      <h1>Jodok's recipe v1.0</h1>
+      <p>
+        Hi, my love!<br />
+        I am happy to start this project for you :)<br />
+        I hope you like this online recipe book, it might reminds you of one of the things we appreciate the most in our lives, cooking together!<br />
+        I love you more than anything
+      </p>
+      <div>
+        <Ramen />
+      </div>
+      
+    </>
+  )
+}
---
+++ jodok/src/recipes/Ramen.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Ramen() {
+    return(
+        <div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi illum facilis totam amet nulla error dolorum architecto, exercitationem nobis quo iure nisi tenetur excepturi et molestias modi nostrum placeat quod?</p>
+        </div>
+    )
+
+}
