e8fae55452a2134a5a8619efaa47501d7734a09c
Karina <lkarina.nakahara@gmail.com>
2023-07-28 12:05:18 -0300

    peerdep

--- jodok/src/components/organisms/Footer/index.jsx
+++ jodok/src/components/organisms/Footer/index.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export default function Footer(){
     return(
        <footer>
-        <h1>Footer...</h1>
+        <h1>Footer..</h1>
        </footer> 
     )
 }
