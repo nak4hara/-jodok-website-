8ef9fa70e149a3e169d522530f68e38eb16d3b7d
Karina <lkarina.nakahara@gmail.com>
2023-07-28 16:16:49 -0300

    footer changes

--- jodok/src/components/atoms/Logo/index.jsx
+++ jodok/src/components/atoms/Logo/index.jsx
@@ -1,10 +1,12 @@
 import styles from './Logo.module.css'
 import LogoImage from '../../../assets/logo.png'
 
-export default function Logo() {
+export default function Logo({size}) {
     return (
         <div className={styles.logo}>
-            <img src= {LogoImage} alt="Logotype from the website" />
+            <img className={styles.myLogo}
+                src= {LogoImage}
+                alt="Logotype from the website" />
         </div>
     )
 }
--- jodok/src/components/organisms/Footer/index.jsx
+++ jodok/src/components/organisms/Footer/index.jsx
@@ -3,7 +3,9 @@ import styles from './Footer.module.css'
 export default function Footer(){
     return(
        <footer>
-        <h1>Footer..</h1>
+            <section className={`${styles.column} ${styles.socialMedia}`}>
+
+            </section>
        </footer> 
     )
 }
